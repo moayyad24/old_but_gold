c0a7c2f58cdff9c8e33214cc4410d378839a94f4
moayyad <moayyadmmm@gmail.com>
2025-06-05 14:59:26 +0300

    activate login button to navigate to login screen

--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:old_but_gold/core/constants/routes.dart';
 import 'package:old_but_gold/core/theme/app_colors.dart';
 import 'package:old_but_gold/core/theme/app_text_styles.dart';
 import 'package:old_but_gold/core/theme/ui_parameters.dart';
@@ -49,7 +50,9 @@ class OnboardingScreen extends StatelessWidget {
                 12.verticalSpace,
                 AppConfirmButton(
                   text: 'Login',
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.pushNamed(context, Routes.loginScreen);
+                  },
                   border: true,
                   backgroundColor: AppColors.whiteFBFBFB,
                 ),
