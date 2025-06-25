b50979b8d3eb0e5c386c507381a152ee67d62cc4
moayyad <moayyadmmm@gmail.com>
2025-06-25 14:59:19 +0300

    finish bottom app bar design and manage state

---
+++ lib/core/widgets/app_bottom_navigation_bar.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:old_but_gold/core/theme/app_colors.dart';
+
+class AppBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onIndexChanged;
+  const AppBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onIndexChanged,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 85,
+      alignment: Alignment.topCenter,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(top: BorderSide(width: 1, color: AppColors.greyB7B7B7)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _NavBarItem(
+            icon: 'assets/icons/house.svg',
+            label: 'Home',
+            index: 0,
+            currentIndex: currentIndex,
+            onTap: onIndexChanged,
+          ),
+          _NavBarItem(
+            icon: 'assets/icons/liked.svg',
+            label: 'Liked',
+            index: 1,
+            currentIndex: currentIndex,
+            onTap: onIndexChanged,
+          ),
+          _NavBarItem(
+            icon: 'assets/icons/add.svg',
+            label: 'Add',
+            index: 2,
+            currentIndex: currentIndex,
+            onTap: onIndexChanged,
+          ),
+          _NavBarItem(
+            icon: 'assets/icons/inbox.svg',
+            label: 'Inbox',
+            index: 3,
+            currentIndex: currentIndex,
+            onTap: onIndexChanged,
+          ),
+          _NavBarItem(
+            icon: 'assets/icons/profile.svg',
+            label: 'Profile',
+            index: 4,
+            currentIndex: currentIndex,
+            onTap: onIndexChanged,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NavBarItem extends StatelessWidget {
+  final String icon;
+  final String label;
+  final int index;
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const _NavBarItem({
+    required this.icon,
+    required this.label,
+    required this.index,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isSelected = index == currentIndex;
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: AnimatedContainer(
+        height: 67,
+        width: 75,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        decoration: BoxDecoration(
+          gradient:
+              isSelected
+                  ? const LinearGradient(
+                    colors: [
+                      Color(0xFFFFDC91),
+                      Color(0xFFFFDC90),
+                      Color(0xFFFFFFFF),
+                    ],
+                    stops: [0, 0.76, 1],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  )
+                  : null,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(icon),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:old_but_gold/core/theme/app_colors.dart';
-import 'package:old_but_gold/features/home/ui/widgets/home_sliver_body.dart';
-import 'package:old_but_gold/features/home/ui/widgets/home_top_section.dart';
+import 'package:old_but_gold/core/widgets/app_bottom_navigation_bar.dart';
+import 'package:old_but_gold/features/home/ui/home_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,84 +10,35 @@ class MainScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
+  final ValueNotifier<int> _currentIndexNotifier = ValueNotifier(0);
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const Center(child: Text('Liked Page', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Add Page', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Inbox Page', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Profile Page', style: TextStyle(fontSize: 24))),
+  ];
+  @override
+  void dispose() {
+    _currentIndexNotifier.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(slivers: [HomeSliverAppBar(), HomeSliverBody()]),
-      bottomNavigationBar: _buildCustomBottomBar(),
-    );
-  }
-
-  Widget _buildCustomBottomBar() {
-    return Container(
-    height: 85,
-      alignment: Alignment.topCenter,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border(top: BorderSide(width: 1, color: AppColors.greyB7B7B7)),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          _buildNavItem(Icons.home, 'Home', 0),
-          _buildNavItem(Icons.favorite, 'Liked', 1),
-          _buildNavItem(Icons.add_circle, 'Add', 2),
-          _buildNavItem(Icons.inbox, 'Inbox', 3),
-          _buildNavItem(Icons.person, 'Profile', 4),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildNavItem(IconData filledIcon, String label, int index) {
-    final isSelected = _currentIndex == index;
-
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _currentIndex = index;
-        });
+    return ValueListenableBuilder(
+      valueListenable: _currentIndexNotifier,
+      builder: (context, index, _) {
+        return Scaffold(
+          body: _screens[index],
+          bottomNavigationBar: AppBottomNavigationBar(
+            currentIndex: index,
+            onIndexChanged:
+                (newIndex) => _currentIndexNotifier.value = newIndex,
+          ),
+        );
       },
-      child: AnimatedContainer(
-        height: 65,
-        width: 75,
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.linear,
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        decoration: BoxDecoration(
-          gradient:
-              isSelected
-                  ? const LinearGradient(
-                    colors: [
-                      Color(0xFFFFDC91),
-                      Color(0xFFFFDC90),
-                      Color(0xFFFFFFFF),
-                    ],
-                    stops: [0, 0.76, 1],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  )
-                  : null,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(filledIcon, size: 24, color: Colors.black),
-            const SizedBox(height: 4),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
