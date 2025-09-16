12301e1346ea2faa92d83e955de14ed8631fca65
moayyad <moayyadmmm@gmail.com>
2025-09-16 20:23:28 +0300

    Refactor/ categories design in home screen

--- lib/features/home/ui/widgets/categories_list_view.dart
+++ lib/features/home/ui/widgets/categories_list_view.dart
@@ -9,34 +9,59 @@ class CategoriesListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 100.h,
+      height: 110.h,
       child: ListView.builder(
         padding: EdgeInsets.symmetric(horizontal: 16),
         scrollDirection: Axis.horizontal,
         itemCount: 7,
         itemBuilder: (context, index) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 74.h,
-                width: 74.w,
-                margin: EdgeInsets.symmetric(horizontal: 6),
-                padding: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: AppColors.mainFFE09C,
-                  shape: BoxShape.circle,
-                  border: Border.all(color: AppColors.grey666666.withAlpha(50)),
-                ),
-                child: SvgPicture.asset(
-                  'assets/icons/onboarding/onboarding_$index.svg',
-                ),
-              ),
-              Text('Gaming', style: AppTextStyles.semiBold15),
-            ],
+          return CategoryCard(
+            title: 'Gaming',
+            iconPath: 'assets/icons/onboarding/onboarding_$index.svg',
+            isActive: index == 0 ? true : false,
           );
         },
       ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String iconPath;
+  final bool isActive;
+  const CategoryCard({
+    super.key,
+    required this.title,
+    required this.iconPath,
+    required this.isActive,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: 75.h,
+          width: 75.w,
+          margin: EdgeInsets.symmetric(horizontal: 6),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: isActive ? AppColors.mainFFE09C : Color(0xFFFFE8B2),
+            borderRadius: BorderRadius.circular(12),
+            border: isActive
+                ? Border.all(
+                    color: AppColors.grey666666.withAlpha(50),
+                    width: 2,
+                  )
+                : null,
+          ),
+          child: SvgPicture.asset(iconPath),
+        ),
+        10.verticalSpace,
+        Text(title, style: AppTextStyles.semiBold15),
+      ],
     );
   }
 }
